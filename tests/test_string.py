a46b1e5d8efbcf7d61bae3f75ed8d2ea2afd97ca
ualikhansarsen <u.sarsenbayev.kazdream.kz>
2022-06-09 01:03:05 +0600

    add teardown methods

--- tests/test_string.py
+++ tests/test_string.py
@@ -4,7 +4,24 @@ from src import Step
 
 @TestScenario.init()
 class TestString:
+    @TestScenario.before_all
+    def set_up(self):
+        print('Set Up')
+
+    @TestScenario.after_all
+    def clear(self):
+        print('close connection')
+
     def test_conct(self):
+        with Step("Авторизация"):
+            a = "v"
+            assert 'a' == 'a'
+        with Step('Запрос'):
+            assert "v" == a
+        with Step('Проверка схемы'):
+            assert "b" == a
+
+    def test_name(self):
         with Step("Авторизация"):
             a = "v"
             assert 'a' == 'a'
