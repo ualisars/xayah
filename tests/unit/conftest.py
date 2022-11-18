d3e96fee7e05cf77c963e82f65ccd5a831973ded
ualikhansarsen <u.sarsenbayev.kazdream.kz>
2022-11-18 12:02:45 +0600

    add several test suite add params test

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,5 +1,6 @@
 from pytest import fixture
 from src.xayah.test_suite import TestSuite
+from src.xayah.test_case import TestCase
 
 
 @fixture(scope='function')
@@ -13,3 +14,30 @@ def test_suite_title_result(test_result):
     ClassTestSuiteTitle.run_test_cases()
     result = test_result.create_test_result()
     return result.get(ClassTestSuiteTitle.__name__)
+
+
+@fixture(scope='function')
+def several_before_all(test_result):
+
+    @TestSuite.init
+    class FirstBeforeAll:
+        @TestSuite.before_all
+        def set_up(self):
+            TestCase.add_additional_params(FirstBeforeAll, self.test_before_all, {'name': 'first before all'})
+
+        def test_before_all(self):
+            pass
+
+    @TestSuite.init
+    class SecondBeforeAll:
+        @TestSuite.before_all
+        def set_up(self):
+            TestCase.add_additional_params(SecondBeforeAll, self.test_before_all, {'name': 'second before all'})
+
+        def test_before_all(self):
+            pass
+
+    FirstBeforeAll.run_test_cases()
+    SecondBeforeAll.run_test_cases()
+
+    return test_result.create_test_result()
