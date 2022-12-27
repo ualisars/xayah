44d5ba6eb999a69e53f201af5592915b51b2c83d
ualikhansarsen <u.sarsenbayev.kazdream.kz>
2022-12-27 16:32:39 +0600

    add shared data

---
+++ src/xayah/shared_data.py
@@ -0,0 +1,41 @@
+from inspect import stack
+
+
+class SharedData:
+    module_shared_data: dict = {}
+    global_shared_data: dict = {}
+
+    @staticmethod
+    def share(key: str, value: any, scope: str = 'module') -> None:
+        """
+        add data to shared data which can be used by other test suites
+        :param key: name of the shared data
+        :param value: value of the shared data
+        :param scope: scope of the shared data (can be local and global)
+        """
+        module = stack()[1][1]
+        if scope == 'module':
+            SharedData.module_shared_data[key] = {'module': module, 'value': value}
+        elif scope == 'global':
+            SharedData.global_shared_data[key] = {'value': value}
+        else:
+            raise ValueError(f'scope can only be module or global, get: {scope}')
+
+    @staticmethod
+    def get(key: str) -> any:
+        """
+        get share data from other test classes
+        :param key: name of the shared data
+        :return shared_data
+        """
+        module = stack()[1][1]
+
+        data = SharedData.module_shared_data.get(key)
+        if data and data.get('module') == module and data.get('value'):
+            return data.get('value')
+
+        data = SharedData.global_shared_data.get(key)
+        if data.get('value'):
+            return data.get('value')
+        else:
+            return None
--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,7 @@
 from pytest import fixture
 from src.xayah.test_suite import TestSuite
 from src.xayah.test_case import TestCase
+from src.xayah.shared_data import SharedData
 
 
 @fixture(scope='function')
@@ -18,7 +19,6 @@ def test_suite_title_result(test_result):
 
 @fixture(scope='function')
 def several_before_all(test_result):
-
     @TestSuite.init
     class FirstBeforeAll:
         @TestSuite.before_all
@@ -41,3 +41,41 @@ def several_before_all(test_result):
     SecondBeforeAll.run_test_cases()
 
     return test_result.create_test_result()
+
+
+@fixture(scope='function')
+def shared_data_inside_class(test_result):
+    @TestSuite.init
+    class TestClass:
+        @TestSuite.before_all
+        def initialize(self):
+            SharedData.share('driver', 'firefox')
+
+        def test_shared_data(self):
+            driver = SharedData.get('driver')
+            assert driver == 'firefox'
+
+    TestClass.run_test_cases()
+    result = test_result.create_test_result()
+    return result.get(TestClass.__name__)
+
+
+@fixture(scope='function')
+def shared_data_between_test_suites(test_result):
+    @TestSuite.init
+    class TestSuite1:
+        def test_shared_data(self):
+            SharedData.share('driver', 'chrome')
+            assert 1 == 1
+
+    @TestSuite.init
+    class TestSuite2:
+        def test_shared_data(self):
+            driver = SharedData.get('driver')
+            assert driver == 'chrome'
+
+    TestSuite1.run_test_cases()
+    TestSuite2.run_test_cases()
+
+    result = test_result.create_test_result()
+    return result.get(TestSuite2.__name__)
